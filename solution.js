ac9452d0bf2475259a6a3f4278ae79c1f16b489f
Chantal <chantalgomez@pursuit.org>
2023-09-24 22:26:20 -0400

    Half the problems done

--- solution.js
+++ solution.js
@@ -1,5 +1,146 @@
 const { nums, words } = require("./data/data.js");
 
+//class like componets are meant to be capitalized
+class Node {
+  //these are the properties of the class
+  constructor(data, next, head) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+
+  //these are called methods ----- work just like function
+
+  //insert at the head of the list
+  insert(data) {
+    //get the head of the list
+    let currentFirstNode = this.head;
+    //creat a new node
+    let newNode = new Node(data);
+
+    if (!currentFirstNode) {
+      //if there is no head(first node on the list) new node is now the head
+      currentFirstNode = newNode;
+    } else {
+      //the new Node's(the node you want to insert) next section is now equal
+      //to the head of the list you want to add to
+      newNode.next = currentFirstNode;
+
+      //asign the node you inserted the value of the current first node (the head)
+      currentFirstNode = newNode;
+    }
+  }
+
+  // size
+  size() {
+    let count = 0;
+    let currentNode = this.head;
+
+    while (currentNode) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    return count;
+  }
+
+  delete(data) {
+    // Is the list empty?
+    if (!this.head) return console.log("List is empty!");
+
+    // get the data of the node you want to delete(left side of the box chart)
+    let nodeData = this.data;
+
+    //look throught the array to find the node you are looking for
+    // If the currentNode doesn't match the data and it has a next value,
+    // increase the counter & move to the next node
+    while (currentNode.data !== data && currentNode.next) {
+      counter++;
+      currentNode = currentNode.next;
+    }
+
+    // Store the node that matches the data in a new variable
+    let foundNode = currentNode;
+
+    // Reset current node to the first node in the list
+    currentNode = this.head;
+
+    // finding the location of the found node
+    for (let i = 1; i < counter; i++) {
+      currentNode = currentNode.next;
+    }
+    // "Sep".next = "Oct".next
+    // "Sept".next => "Nov"
+    currentNode.next = foundNode.next;
+    console.log(currentNode.next);
+  }
+
+  getFirst(data) {
+    // if( this.data === this.head){
+    //   return head
+    // }
+    if (!this.head) return null;
+
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) return console.log("this list is empty!");
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+
+  search(key) {
+    let currentNode = this.head
+    while( currentNode){
+      if(currentNode.data === key){
+        return currentNode
+      }
+      currentNode = currentNode.next
+    }
+  }
+
+  getKth(){
+
+  }
+
+  getKthToLast(){
+
+  }
+
+  isEmpty(){
+    if(this.head === undefined){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  clear(){
+    this.head = null;
+  }
+
+  toArray(){
+
+  }
+
+  containsDuplicates(){
+
+  }
+
+}
+
 module.exports = {
   Node,
   LinkedList,
