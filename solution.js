c921705f70fe73857294a6c563234e2452a0186e
davep80 <davidpaq1@gmail.com>
2023-09-25 00:04:07 -0400

    3 methods

--- solution.js
+++ solution.js
@@ -1,5 +1,47 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+
+  insert(data) {
+    let temp = new Node(data);
+    temp.next = this.head;
+    this.head = temp;
+  }
+
+  size() {
+    let count = 0;
+    let head = this.head;
+    while (head && ++count) head = head.next;
+    return count;
+  }
+
+  delete(key) {
+    let curr = this.head;
+    if (curr && curr.data === key) {
+      this.head = curr.next;
+      return;
+    }
+
+    while (curr && curr.next) {
+      if (curr.next.data === key) {
+        curr.next = curr.next.next;
+        return;
+      }
+      curr = curr.next;
+    }
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
