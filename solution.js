f8e4d7dd559e4b2627df0682f37762e8cf5915a4
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-17 21:06:22 -0500

    builds out linkedlist start

--- solution.js
+++ solution.js
@@ -1,5 +1,40 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+const firstNode = new Node(1);
+const secondNode = new Node(2);
+firstNode.next = secondNode;
+
+// console.log(firstNode);
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
+const wordList = new LinkedList(firstNode);
+
+
+
+
+
+
+
 module.exports = {
   Node,
   LinkedList,
