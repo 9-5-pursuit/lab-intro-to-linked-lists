680ec6ae836b5923a8029cb0d48365cf2f494e9a
Nicole Slater <nicoleslater@pursuit.org>
2024-01-17 20:49:26 -0500

    Fixing Syntax errors

--- solution.js
+++ solution.js
@@ -1,150 +1,146 @@
 const { nums, words } = require("./data/data.js");
 
 class Node {
-  constructor(data, next = null){
+  constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
 }
 
-class LinkedList{
-  constructor(){
+class LinkedList {
+  constructor() {
     this.head = null;
-
   }
-}
 
-insert(data); {
-  this.head = new Node(data, this.head);
-}
+  insert(data) {
+    this.head = new Node(data, this.head);
+  }
 
-size(); {
-  let count = 0;
-  let node = this.head;
+  size() {
+    let count = 0;
+    let node = this.head;
 
-  while(node){
-    count++;
-    node = node.next
+    while (node) {
+      count++;
+      node = node.next;
+    }
+
     return count;
   }
-  // return count;
-}
 
-delete(data);{
-  if(!this.head){
-    return;
-  }
-
-  if(this.head.data === key){
-    this.head = this.head.next;
-    return;
-  }
-
-  let current = this.head;
-  let prev = null; 
-
-  while(current && current.data !== key){
-    prev = current;
-    current = current.next;
-  }
-
-  if(!current){
-    return;
-  }
-
-  prev.next = current.next;
-}
-
-getFirst(); {
-  return this.head ? this.head.data : null;
-}
-
-getLast() ;{
-  let current = this.head;
-  while (current && current.next) {
-    current = current.next;
-  }
-  return current ? current.data : null;
-}
-
-search(key); {
-  let current = this.head;
-  while (current) {
-    if (current.data === key) {
-      return current.data;
+  delete(key) {
+    if (!this.head) {
+      return;
     }
-    current = current.next;
-  }
-  return null;
-}
 
-getKth(k); {
-  let current = this.head;
-  for (let i = 0; i < k; i++) {
+    if (this.head.data === key) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+    let prev = null;
+
+    while (current && current.data !== key) {
+      prev = current;
+      current = current.next;
+    }
+
     if (!current) {
-      return null;
+      return;
     }
-    current = current.next;
+
+    prev.next = current.next;
   }
-  return current ? current.data : null;
-}
 
-getKthToLast(k) ;{
-  let slow = this.head;
-  let fast = this.head;
+  getFirst() {
+    return this.head ? this.head.data : null;
+  }
 
-  for (let i = 0; i < k; i++) {
-    if (!fast) {
-      return null;
+  getLast() {
+    let current = this.head;
+    while (current && current.next) {
+      current = current.next;
     }
-    fast = fast.next;
+    return current ? current.data : null;
   }
 
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next;
-  }
-
-  return slow ? slow.data : null;
-}
-
-isEmpty(); {
-  return this.head === null;
-}
-
-clear(); {
-  this.head = null;
-}
-
-toArray(); {
-  let result = [];
-  let current = this.head;
-
-  while (current) {
-    result.push(current.data);
-    current = current.next;
-  }
-
-  return result;
-}
-
-containsDuplicates();{
-  let seen = new Set();
-  let current = this.head;
-
-  while (current) {
-    if (seen.has(current.data)) {
-      return true;
+  search(key) {
+    let current = this.head;
+    while (current) {
+      if (current.data === key) {
+        return current.data;
+      }
+      current = current.next;
     }
-    seen.add(current.data);
-    current = current.next;
+    return null;
   }
 
-  return false;
+  getKth(k) {
+    let current = this.head;
+    for (let i = 0; i < k; i++) {
+      if (!current) {
+        return null;
+      }
+      current = current.next;
+    }
+    return current ? current.data : null;
+  }
+
+  getKthToLast(k) {
+    let slow = this.head;
+    let fast = this.head;
+
+    for (let i = 0; i < k; i++) {
+      if (!fast) {
+        return null;
+      }
+      fast = fast.next;
+    }
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow ? slow.data : null;
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  toArray() {
+    let result = [];
+    let current = this.head;
+
+    while (current) {
+      result.push(current.data);
+      current = current.next;
+    }
+
+    return result;
+  }
+
+  containsDuplicates() {
+    let seen = new Set();
+    let current = this.head;
+
+    while (current) {
+      if (seen.has(current.data)) {
+        return true;
+      }
+      seen.add(current.data);
+      current = current.next;
+    }
+
+    return false;
+  }
 }
-
-
-
 
 
 
