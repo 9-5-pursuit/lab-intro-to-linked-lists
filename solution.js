4046dfbafe2c9eb22d0df2ba2cc5ed91e6ef1802
Nicole Slater <nicoleslater@pursuit.org>
2024-01-17 19:59:29 -0500

    Add Node Class to solution

--- solution.js
+++ solution.js
@@ -1,5 +1,24 @@
 const { nums, words } = require("./data/data.js");
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
 
+const words = [
+  "the",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+
+class Node {
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
+}
 module.exports = {
   Node,
   LinkedList,
