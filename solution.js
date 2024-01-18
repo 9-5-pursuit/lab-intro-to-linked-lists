39fb571ce9db0e6b91aa39a800a32d630305628b
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-17 21:51:51 -0500

    removes code from linkedlist, and begins to rebuild

--- solution.js
+++ solution.js
@@ -7,75 +7,29 @@ class Node {
   }
 }
 
-
-
-// console.log(firstNode);
-
 class LinkedList {
   constructor(head = null) {
       this.head = head
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
   }
 
   size() {
     let count = 0;
     let node = this.head;
     while (node) {
-        count++;
-        node = node.next;
+      count++;
+      node = node.next;
     }
     return count;
-}
+  }
 
  
-  search(value) {
-      
-      let currentNode = this.head;
-      while (currentNode !== null && currentNode.data !== value) {
-          currentNode = currentNode.next
-      }
-      return currentNode
-  }
-
-  clear() {
-      this.head = null;
-  }
-  
-  getFirst() {
-      return this.head;
-  }
-  
-  getLast () {
-      let lastNode = this.head;
-      if(lastNode) {
-        while (lastNode.next) {
-          lastNode = lastNode.next;
-        }
-        return lastNode
-      }
-    }
-
-  
-  insert() {}
-  
-  delete(data) {
-    if (!this.head) {
-        return;
-    }
-
-    if (this.head.data === data) {
-        this.head = this.head.next;
-        return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next && currentNode.next.data !== data) {
-        currentNode = currentNode.next;
-    }
-
-    if (currentNode.next) {
-        currentNode.next = currentNode.next.next;
-    }
-}
+ 
 
 }
 
